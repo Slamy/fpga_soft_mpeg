4463c88b90d43d0da3474d0118e78d72a46e7776
Andre Zeps <andre.zeps@googlemail.com>
2025-08-16 22:39:20 +0200

    dragons lair but P frames are wrong

--- rtl/top_vexii.sv
+++ rtl/top_vexii.sv
@@ -6,8 +6,8 @@ module top_vexii (
     input reset
 );
 
-    localparam SIZE = 16682;
-    bit [31:0] mpeg_video_rom[16682];
+    localparam SIZE = 454125;
+    bit [31:0] mpeg_video_rom[SIZE];
     initial $readmemh("fmv.mem", mpeg_video_rom);
 
     bit [15:0] data_word;
@@ -25,7 +25,7 @@ module top_vexii (
     );
 
     bit provide_lower_word = 0;
-    bit [14:0] mpeg_stream_address = 0;
+    bit [18:0] mpeg_stream_address = 0;
 
     bit [32:0] cnt=0;
 
@@ -33,7 +33,7 @@ module top_vexii (
         data_strobe <= 0;
         cnt <= cnt + 1;
 
-        if (!fifo_full && !reset && mpeg_stream_address <= (SIZE + 10) && cnt[8:0]==0) begin
+        if (!fifo_full && !reset && mpeg_stream_address <= (SIZE + 10)) begin
             data_strobe <= 1;
             provide_lower_word <= !provide_lower_word;
             if (provide_lower_word) mpeg_stream_address <= mpeg_stream_address + 1;
