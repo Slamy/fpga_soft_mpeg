61691c384c24b45e2a612c6b9e264fc9b75146aa
Andre Zeps <andre.zeps@googlemail.com>
2025-08-25 20:42:47 +0200

    wup

--- sw/shared.h
+++ sw/shared.h
@@ -41,7 +41,7 @@ int image_synthesis_buffer_index = 0;
 int image_synthesis_buffer_index2 = 0;
 static int worker_cnt;
 
-#define SHARED_BUFFER_ENTRIES 20
+#define SHARED_BUFFER_ENTRIES 500
 
 struct image_synthesis_descriptor *get_next_synthesis_desc()
 {
@@ -64,7 +64,7 @@ struct image_synthesis_descriptor *get_next_synthesis_desc()
 
     __asm volatile("" : : : "memory");
     
-#if 1
+#if 0
     while (retval->ready !=0)
         __asm volatile("" : : : "memory");
 #endif
